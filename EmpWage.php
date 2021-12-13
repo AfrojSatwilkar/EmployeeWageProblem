9e9143793991aa3fc8372cb200e811e96f1d716e
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-12-13 22:26:43 +0530

    Calculate Wages for a Month

--- EmpWage.php
+++ EmpWage.php
@@ -2,20 +2,28 @@
 echo "Welcome to Employee Wage Problem <br>";
 
 $WAGE_PER_HR = 20;
+$MAX_WORKING_DAYS = 20;
+$TOTAL_WORKING_DAYS = 0;
+$TOTAL_EMP_HRS = 0;
 $EMP_HRS;
-$num = rand(0,2);
 
-switch($num){
-    case 1 :
-        $EMP_HRS = 8;
-        break;
-    case 2 :
-        $EMP_HRS = 4;
-        break;
-    default :
-        $EMP_HRS = 0;
+while ($TOTAL_WORKING_DAYS < $MAX_WORKING_DAYS) {
+    $num = rand(0,2);
+    switch($num){
+        case 1 :
+            $EMP_HRS = 8;
+            break;
+        case 2 :
+            $EMP_HRS = 4;
+            break;
+        default :
+            $EMP_HRS = 0;
+    }
+
+    $TOTAL_EMP_HRS = $TOTAL_EMP_HRS + $EMP_HRS;
+    $TOTAL_WORKING_DAYS++;
 }
 
-$TotalWageInDay = $WAGE_PER_HR * $EMP_HRS;
-echo "Total Wage in day : " . $TotalWageInDay;
+$TOTAL_SALARY = $WAGE_PER_HR * $TOTAL_EMP_HRS;
+echo "Total Salary : " . $TOTAL_SALARY;
 ?>
