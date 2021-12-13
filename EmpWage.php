36679f2f1fae6b0a4d3a2f1d73fd156cdafa5e9a
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-12-13 22:20:26 +0530

    Calculating Daily Wage

--- EmpWage.php
+++ EmpWage.php
@@ -2,10 +2,14 @@
 echo "Welcome to Employee Wage Problem <br>";
 
 $IS_PRESENT = 1;
+$WAGE_PER_HR = 20;
+$EMP_HRS;
 $num = rand(0,1);
 
 if ($num == $IS_PRESENT) 
-    echo "Employee is Present";
+    $EMP_HRS = 8;
 else
-    echo "Employee is Absent";
+    $EMP_HRS = 0;
+$TotalWageInDay = $WAGE_PER_HR * $EMP_HRS;
+echo "Total Wage in day : " . $TotalWageInDay;
 ?>
