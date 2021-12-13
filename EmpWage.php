207cdcb3cc7d6732257f5e5c1b60051714f9a7da
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-12-13 22:16:57 +0530

    checking Employee is present or absent

---
+++ EmpWage.php
@@ -0,0 +1,11 @@
+<?php
+echo "Welcome to Employee Wage Problem <br>";
+
+$IS_PRESENT = 1;
+$num = rand(0,1);
+
+if ($num == $IS_PRESENT) 
+    echo "Employee is Present";
+else
+    echo "Employee is Absent";
+?>
