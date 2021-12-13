ebd0b8f4b6d234011d9aca1a1b7b8e003b1c8949
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-12-13 22:24:42 +0530

    Calculate daily wage using switch case

--- EmpWage.php
+++ EmpWage.php
@@ -1,18 +1,20 @@
 <?php
 echo "Welcome to Employee Wage Problem <br>";
 
-$IS_PRESENT = 1;
-$IS_ABSENT = 2;
 $WAGE_PER_HR = 20;
 $EMP_HRS;
 $num = rand(0,2);
 
-if ($num == $IS_PRESENT) 
-    $EMP_HRS = 8;
-elseif($num == $IS_ABSENT)
-    $EMP_HRS = 4;
-else
-    $EMP_HRS = 0;
+switch($num){
+    case 1 :
+        $EMP_HRS = 8;
+        break;
+    case 2 :
+        $EMP_HRS = 4;
+        break;
+    default :
+        $EMP_HRS = 0;
+}
 
 $TotalWageInDay = $WAGE_PER_HR * $EMP_HRS;
 echo "Total Wage in day : " . $TotalWageInDay;
