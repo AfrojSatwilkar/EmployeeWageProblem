7a92a15f6056fe9a2c47e5bb5f5c5b47442328e2
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-12-23 18:25:33 +0530

    Refactor : Ability to get total wage and daily wage queried by company

--- EmpWage.php
+++ EmpWage.php
@@ -51,17 +51,13 @@ class EmpWage extends EmpWageParentClass implements EmpWageInterface {
                     $EMP_HRS = 0;
             }
             $TOTAL_EMP_HRS = $TOTAL_EMP_HRS + $EMP_HRS;
-            $dailyEmpWage[$TOTAL_WORKING_DAYS] = parent :: $WAGE_PER_HR * $EMP_HRS;
+            $this->dailyEmpWage[$companyWage->company][$TOTAL_WORKING_DAYS] = parent :: $WAGE_PER_HR * $EMP_HRS;
+            
             $TOTAL_WORKING_DAYS++;
-        }
-
-        echo parent :: $company . " Daily Employee Wage :\n";
-        foreach($dailyEmpWage as $key => $dailyWage) {
-            echo "Wage for Day " . $key . " : " . $dailyWage . "\n";
         }
     
         $TOTAL_SALARY = parent :: $WAGE_PER_HR * $TOTAL_EMP_HRS;
-        return "Total Employee wage for company " . parent :: $company . " is : " . $TOTAL_SALARY . "\n";
+        return "Total Employee wage for company " . $companyWage->company . " is : " . $TOTAL_SALARY . "\n";
     }
 
     /**
@@ -70,6 +66,33 @@ class EmpWage extends EmpWageParentClass implements EmpWageInterface {
     public function showCompanyEmpWage() {
         foreach($this->empWage as $company => $companyDetails) {
             echo $companyDetails;
+        }
+    }
+
+    /**
+     * companyWage method is used to print company wage
+     * @param companyName 
+     */
+    public function companyWage($companyName) {
+        foreach($this->empWage as $company => $companyDetails) {
+            if($company == $companyName) {
+                echo $companyDetails;
+            }
+        }
+    }
+
+    /**
+     * dailyWageEmp method is used to print daily wage of emp in company
+     * @param companyName
+     */
+    public function dailyWageEmp($companyName) {
+        foreach($this->dailyEmpWage as $key => $dailyWage) {
+            if($key == $companyName) {
+                for($i = 0; $i < count($dailyWage); $i++) {
+                    // echo $dailyWage[$i] . " ";
+                    echo "Wage for Day " . $i . " : " . $dailyWage[$i] . "\n";
+                }
+            }
         }
     }
 }
--- EmpWageParentClass.php
+++ EmpWageParentClass.php
@@ -1,7 +1,7 @@
 <?php
 class EmpWageParentClass {
 
-    public static $company;
+    public $company;
     public static $WAGE_PER_HR;
     public $MAX_WORKING_DAYS;
     public $MAX_HRS_IN_MONTH;
@@ -10,7 +10,7 @@ class EmpWageParentClass {
      * initialize instance variables
      */
     public function __construct($company, $WAGE_PER_HR, $MAX_WORKING_DAYS, $MAX_HRS_IN_MONTH) {
-        self :: $company = $company;
+        $this->company = $company;
         self :: $WAGE_PER_HR = $WAGE_PER_HR;
         $this->MAX_WORKING_DAYS = $MAX_WORKING_DAYS;
         $this->MAX_HRS_IN_MONTH = $MAX_HRS_IN_MONTH;
